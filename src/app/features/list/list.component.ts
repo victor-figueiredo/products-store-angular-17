4adc7480e63475c1f9cd7eabf5aac8ef2f1b1b92
Victor Figueiredo <vcfo081@gmail.com>
2024-06-29 14:23:27 -0300

    feat: add create product button

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/products.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
