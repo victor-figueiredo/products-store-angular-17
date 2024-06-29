502a87ba91339eb4ad726bb5a5e2c3ae7d63b6a0
Victor Figueiredo <vcfo081@gmail.com>
2024-06-29 14:02:19 -0300

    refactor: create card component for product list

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Product } from '../../../../shared/interfaces/products.interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {
+  product = input.required<Product>();
+
+  productTitle = computed(() => this.product().title);
+}
