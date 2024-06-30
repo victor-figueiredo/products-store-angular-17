82ed99a430399524d34066c046260051609be96d
Victor Figueiredo <vcfo081@gmail.com>
2024-06-29 21:27:31 -0300

    feat: add edit component

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '../../shared/services/products.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Product } from '../../shared/interfaces/products.interface';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './edit.component.html',
-  styleUrl: './edit.component.scss'
+  styleUrl: './edit.component.scss',
 })
 export class EditComponent {
+  productsService = inject(ProductsService);
+  snackbar = inject(MatSnackBar);
+  router = inject(Router);
+  product: Product = inject(ActivatedRoute).snapshot.data['product'];
 
+  form = new FormGroup({
+    title: new FormControl<string>(this.product.title, {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
+
+  onSubmit() {
+    this.productsService
+      .put(this.product.id, { title: this.form.controls.title.value })
+      .subscribe(() => {
+        this.snackbar.open('Produto salvo', 'Ok');
+        this.router.navigateByUrl('/');
+      });
+  }
 }
